ea13b024655ddeffd4a94535844ea2e5c7e37ad7
AryanNair007 <114175669+AryanNair007@users.noreply.github.com>
2024-07-27 21:34:19 +0530

    Backend code

---
+++ FullStack/restfullwebservices/restfullwebservices/src/main/resources/data.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO TODO(id, username, description, target_date, done) 
+VALUES (10001, 'alex', 'Learn the thing', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10002, 'alex', 'Learn the Java Interview Ques', current_date(), false);
+ 
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10003, 'alex', 'Learn the Spring Interview Ques', current_date(), false);
+ 
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10004, 'alex', 'Learn the Rest API Interview Ques', current_date(), false);
+ 
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10005, 'alex', 'Learn the Security Interview Ques', current_date(), false);
+ 
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10006, 'alex', 'Learn the Jpa Interview Ques', current_date(), false);
